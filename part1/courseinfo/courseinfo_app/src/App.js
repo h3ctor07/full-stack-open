d4089044027a96ba486459f84d02a13adbcfde23
Hector Peñaloza <hector.penaloza07@gmail.com>
2022-09-06 23:26:35 -0500

    finished exercise 1.4 and 1.5

--- part1/courseinfo/courseinfo_app/src/App.js
+++ part1/courseinfo/courseinfo_app/src/App.js
@@ -1,25 +1,28 @@
 const App = () =>{
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  const parts = [part1, part2, part3]
+  
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
@@ -45,6 +48,7 @@ const Part = (props) =>{
 }
 
 const Total = (props) =>{
+  console.log(props)
   return <p>Number of exercises: {props.parts.reduce((partialSum, a) => partialSum + a.exercises, 0)}</p>
 }
 export default App;
