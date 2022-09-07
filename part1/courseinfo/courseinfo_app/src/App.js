12342614c2d136e0f8a241d5c1410e6661ad1c84
Hector Peñaloza <hector.penaloza07@gmail.com>
2022-09-06 23:18:41 -0500

    finished exercise 1.3

--- part1/courseinfo/courseinfo_app/src/App.js
+++ part1/courseinfo/courseinfo_app/src/App.js
@@ -1,19 +1,25 @@
 const App = () =>{
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
   const parts = [part1, part2, part3]
-  const exercises = [exercises1, exercises2, exercises3]
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={parts} exercises={exercises} />
-      <Total exercises={exercises} />
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
@@ -25,20 +31,20 @@ const Header = (props) =>{
 const Content = (props) =>{
   return (
     <>
-      <Part index={0} this={props}/>
-      <Part index={1} this={props}/>
-      <Part index={2} this={props}/>
+      <Part index={0} parts={props.parts}/>
+      <Part index={1} parts={props.parts}/>
+      <Part index={2} parts={props.parts}/>
     </>
   )
 }
 
-const Total = (props) =>{
-  return <p>Number of exercises: {props.exercises.reduce((partialSum, a) => partialSum + a, 0)}</p>
-}
-export default App;
-
 const Part = (props) =>{
   return(
-    <p>{props.this.parts[props.index]} {props.this.exercises[props.index]}</p>
+    <p>{props.parts[props.index].name} {props.parts[props.index].exercises}</p>
   )
 }
+
+const Total = (props) =>{
+  return <p>Number of exercises: {props.parts.reduce((partialSum, a) => partialSum + a.exercises, 0)}</p>
+}
+export default App;
