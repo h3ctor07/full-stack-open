0d208150a2b16b8961c4c1180312e80de5208f4f
Hector Peñaloza <hector.penaloza07@gmail.com>
2022-09-16 12:14:33 -0500

    2.6. Phonebook: able to add names on the phonebook using controlled form

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Person = ({person}) => <p>{person.name}</p>
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas', id: 1},
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) =>{
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: persons.length + 1
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+  return(
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleNewName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>debug: {newName}</div>
+      {persons.map(person =>{
+
+        return <Person key={person.id} person={person}/> 
+      }
+         
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
