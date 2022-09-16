f51f7fbf6e2d524ac990edb19695dc0e06821c0c
Hector Peñaloza <hector.penaloza07@gmail.com>
2022-09-15 23:40:59 -0500

    courseinfo app step 7-8: added total of exercises using reduce method

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,15 +1,22 @@
 const Part = ({part}) =>(
-  <li>{part.name} {part.exercises}</li>
+  <p>{part.name} {part.exercises}</p>
 )
 
-const Content = ({parts}) =>(
-  <ul>
+const Content = ({parts}) =>{
+
+  const total = parts.reduce((sum, part) =>
+    sum + part.exercises
+    ,0
+  )
+
+  return (
+  <div> 
     {parts.map(part =>
       <Part key={part.id} part={part} />
     )}
-
-  </ul>
-)
+    <p><strong>total of {total} exercises</strong></p>
+  </div>
+)}
 
 const Header = ({name}) =>(
     <h1>{name}</h1>
