48d1a9e8d69c84bf9e3a0d8906f4c303229363af
Hector Peñaloza <hector.penaloza07@gmail.com>
2022-09-15 23:56:34 -0500

    2.5. courseinfo app: seperated Course component into its own module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,36 +1,4 @@
-const Part = ({ part }) => (
-  <p>{part.name} {part.exercises}</p>
-)
-
-const Content = ({ parts }) => {
-
-  const total = parts.reduce((sum, part) =>
-    sum + part.exercises
-    , 0
-  )
-
-  return (
-    <div>
-      {parts.map(part =>
-        <Part key={part.id} part={part} />
-      )}
-      <p><strong>total of {total} exercises</strong></p>
-    </div>
-  )
-}
-
-const Header = ({ name }) => (
-  <h1>{name}</h1>
-)
-
-const Course = ({ courses }) => (
-  courses.map(course =>(
-    <div key={course.id}>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-  </div>
-  ))
-)
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
